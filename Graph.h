33d413a4abdfb1912cc0d2463ee4bf932ca3fd7b
Ahmed Hassan Ibrahim <102674308+TyranZilla@users.noreply.github.com>
2023-02-03 14:41:20 +0200

    Create Graph.h
    The header file for Class Graph

---
+++ Graph.h
@@ -0,0 +1,20 @@
+#include "XMLDocument.h"
+
+class Graph{
+private:
+    int NumVertices;
+    bool **edges;
+public:
+    Graph(int NumVertices);
+    ~Graph();
+    bool has_edge(unsigned int from_node, unsigned int to_node);
+    bool add_edge(unsigned int from_node, unsigned int to_node);
+    bool Dotwrite(const char *path);
+    int count_edge(unsigned int to_node);
+    void most_active();
+    std::vector<int> are_mutual(unsigned int u1, unsigned int u2);
+    void mutual_followers(unsigned int u1, unsigned int u2);
+    std::vector<int> are_not_mutual(unsigned int u1);
+    void suggested_followers(unsigned int user);
+    friend class XMLDocument;
+};
